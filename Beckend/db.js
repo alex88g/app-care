5eb00afd57c5febc0a188d3503414234430a1587
alex88g <alexander.gallorini@gmail.com>
2025-04-16 21:17:30 +0200

    test20 done

--- Beckend/db.js
+++ Beckend/db.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Anslut till Railway MySQL via fullständig URL
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-// Exportera connection poolen som Promise API
 module.exports = pool.promise();
