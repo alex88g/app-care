9d5b6de92c74de66bf2539590d80559f78e95835
alex88g <alexander.gallorini@gmail.com>
2025-04-16 22:16:22 +0200

    all structure with database MySQL and Frontend checked and done

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist', 
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    }
-  },
- 
-  server: {
-    fs: {
-      allow: ['..'] 
-    }
-  }
 })
