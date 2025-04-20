6e3755a12bf59f90e23e4eb397b544fbf4182e44
alex88g <alexander.gallorini@gmail.com>
2025-04-20 15:06:38 +0200

    Vercel deployment test

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = '/api'; 
-
+const API_BASE = import.meta.env.VITE_API_URL + '/api';
 const formatPhone = (phone) => {
   return phone.startsWith('+46') ? phone : '+46' + phone.replace(/^0/, '');
 };
